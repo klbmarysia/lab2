d8e0bd4e84ee0ecb079ed86c6d34b9c8ae83bc4d
klbmarysia <Mkalabinska76@gmail.com>
2025-10-19 19:08:12 +0200

    w domu

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,11 +1,27 @@
-﻿// lab2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿
 
 #include <iostream>
+#include <stdio.h>
+    using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+// zd. 5, na celciuszach beądą zera, bo nie będzie wiedział że ma brać z liczb wymiernych nawet jak jest  float
+// zd.6 
+int main(){
+
+    int fahr, celsius, start, limit, krok;
+    start = 0;
+    cout << " podaj limit:  ";
+    cin >> limit;
+    cout << "podaj krok";
+    cin >> krok;
+
+    for (fahr = 0.0; fahr <= limit; fahr = fahr + krok)
+    {
+        float celsius = (5.0 / 9.0) * (fahr - 32.0);
+        cout << fahr << "\t" << celsius << endl;
+    }
+    
 }
 
 
